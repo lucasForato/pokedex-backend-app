f4c2b8138f8eabadab579ccd5f734961c86c5d4a
lucasForato <lucas7forato@gmail.com>
2021-11-30 12:09:46 -0300

    url

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 var pgp = require("pg-promise")();
 
-const cn = process.env.DATABASE_URL;
+const cn = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+};
 
-const db = pgp({ cn });
+const db = pgp(cn);
 
 module.exports = db;
