e4faa4e32bda22ad3dee414ee92621e2cf2c043a
lucasForato <lucas7forato@gmail.com>
2021-11-30 11:52:26 -0300

    hanged port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const userRoutes = require("./routes/userRoutes");
 const deckRoutes = require("./routes/deckRoutes");
 var cors = require("cors");
