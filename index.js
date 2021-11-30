c80aa58746bc533c524208e23059217b5ac24d4a
lucasForato <lucas7forato@gmail.com>
2021-11-30 14:12:09 -0300

    err

--- index.js
+++ index.js
@@ -15,9 +15,4 @@ app.use(express.json()); //have acces to request.body
 app.use("/api/users", userRoutes);
 app.use("/api/decks", deckRoutes);
 
-app.use(express.static(path.join(__dirname, "public")))
-    .set("views", path.join(__dirname, "views"))
-    .set("view engine", "ejs")
-    .get("/", (req, res) => res.render("pages/index"))
-    .get("/cool", (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
