60b711adf30f4fa50fafcc5417492280be572b0a
lucasForato <lucas7forato@gmail.com>
2021-11-30 12:19:34 -0300

    new error

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 require("dotenv").config();
 var pgp = require("pg-promise")();
 
-const cn = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
-};
+const cn = process.env.DATABASE_URL;
 
-const db = pgp(cn);
+const db = pgp({ cn });
 
 module.exports = db;
