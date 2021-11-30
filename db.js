8db35d118614892b20aae65bdf301b7bfd8af96d
lucasForato <lucas7forato@gmail.com>
2021-11-30 12:37:13 -0300

    commit

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 var pgp = require("pg-promise")();
 
-const cn = process.env.DATABASE_URL;
+const cn = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    max: 20, // use up to 30 connections
+};
 
-const db = pgp({ cn });
+const db = pgp(cn);
 
 module.exports = db;
